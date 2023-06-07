d9750398bed0417eb7a8f2934c487828b03ba858
nickonator12345 <109287253+nickonator12345@users.noreply.github.com>
2023-06-07 15:25:59 -0400

    Add files via upload

---
+++ ############### Main prompt ############.py
@@ -0,0 +1,144 @@
+############### Main prompt ########################
+char_level = input("What level are they now?")
+
+char_name = input("is this for Kevin, Sergil, or Nick??")
+
+####################################################
+
+###############  Kevin  #############################
+if char_level == '30' and char_name == 'Kevin':
+    print("rock-slide")
+if char_level == '31' and char_name == 'Kevin':
+    print("none")
+if char_level == '32' and char_name == 'Kevin':
+    print("+ 1 attack ")
+if char_level == '33' and char_name == 'Kevin':
+    print("none")
+if char_level == '34' and char_name == 'Kevin':
+    print("none")
+if char_level == '35' and char_name == 'Kevin':
+    print("none")
+if char_level == '36' and char_name == 'Kevin':
+    print("+ 1 attack")
+if char_level == '37' and char_name == 'Kevin':
+    print("pedal dance")
+if char_level == '38' and char_name == 'Kevin':
+    print("+ 1 defense ")
+if char_level == '39' and char_name == 'Kevin':
+    print("none")
+if char_level == '40' and char_name == 'Kevin':
+    print("+ 1 speed ")
+if char_level == '41' and char_name == 'Kevin':
+    print("none")
+if char_level == '42' and char_name == 'Kevin':
+    print("stone edge")
+if char_level == '43' and char_name == 'Kevin':
+    print("none")
+if char_level == '44' and char_name == 'Kevin':
+    print("none")
+if char_level == '45' and char_name == 'Kevin':
+    print("+1 attack and defense, wood hammer")
+if char_level == '46' and char_name == 'Kevin':
+    print("none")
+if char_level == '47' and char_name == 'Kevin':
+    print("+1 defense ")
+if char_level == '48' and char_name == 'Kevin':
+    print("iron head ")
+if char_level == '49' and char_name == 'Kevin':
+    print("earth quake ")
+if char_level == '50' and char_name == 'Kevin':
+    print("+1 defense , coco rumble, grass, damages all defending pokemon ")
+
+
+
+
+
+###############  Sergil  #############################
+if char_level == '30' and char_name == 'Sergil':
+    print('test')
+if char_level == '31' and char_name == 'Sergil':
+    print('+ 1 speed ')
+if char_level == '32' and char_name == 'Sergil':
+    print('test')
+if char_level == '33' and char_name == 'Sergil':
+    print('+1 speed, sunny day')
+if char_level == '34' and char_name == 'Sergil':
+    print('test')
+if char_level == '35' and char_name == 'Sergil':
+    print(' +1 speed ')
+if char_level == '36' and char_name == 'Sergil':
+    print('test')
+if char_level == '37' and char_name == 'Sergil':
+    print('test')
+if char_level == '38' and char_name == 'Sergil':
+    print('+1 attack ')
+if char_level == '39' and char_name == 'Sergil':
+    print('test')
+if char_level == '40' and char_name == 'Sergil':
+    print('+1 defense ')
+if char_level == '41' and char_name == 'Sergil':
+    print('test')
+if char_level == '42' and char_name == 'Sergil':
+    print('flame thrower')
+if char_level == '43' and char_name == 'Sergil':
+    print('test')
+if char_level == '44' and char_name == 'Sergil':
+    print('overheat')
+if char_level == '45' and char_name == 'Sergil':
+    print('+2 attack')
+if char_level == '46' and char_name == 'Sergil':
+    print('test')
+if char_level == '47' and char_name == 'Sergil':
+    print('+1 attack')
+if char_level == '48' and char_name == 'Sergil':
+    print('aerial ace ')
+if char_level == '49' and char_name == 'Sergil':
+    print('mega horn ')
+if char_level == '50' and char_name == 'Sergil':
+    print('+1 attack, sapping glow fire- pl 33 +30 hp to blazearch ')
+
+
+  
+  ###############  Nick  #############################
+if char_level == '30' and char_name == 'Nick':
+    print('test')
+if char_level == '31' and char_name == 'Nick':
+    print('test')
+if char_level == '32' and char_name == 'Nick':
+    print('test')
+if char_level == '33' and char_name == 'Nick':
+    print('test')
+if char_level == '34' and char_name == 'Nick':
+    print('+ 1 speed, iron tail / bubble blast -water pl:18- flinch ')
+if char_level == '35' and char_name == 'Nick':
+    print('+ 1 attack. +1 defense ')
+if char_level == '36' and char_name == 'Nick':
+    print(' crunch ')
+if char_level == '37' and char_name == 'Nick':
+    print('test')
+if char_level == '38' and char_name == 'Nick':
+    print('slash ')
+if char_level == '39' and char_name == 'Nick':
+    print('+1 defense ')
+if char_level == '40' and char_name == 'Nick':
+    print('iron defense ')
+if char_level == '41' and char_name == 'Nick':
+    print('+ 1 speed ')
+if char_level == '42' and char_name == 'Nick':
+    print('test')
+if char_level == '43' and char_name == 'Nick':
+    print('Dive')
+if char_level == '44' and char_name == 'Nick':
+    print('+1 speed')
+if char_level == '45' and char_name == 'Nick':
+    print('Dark pulse')
+if char_level == '46' and char_name == 'Nick':
+    print('test')
+if char_level == '47' and char_name == 'Nick':
+    print('test')
+if char_level == '48' and char_name == 'Nick':
+    print('+1 defense, aqua tail  ')
+if char_level == '49' and char_name == 'Nick':
+    print('test')
+if char_level == '50' and char_name == 'Nick':
+    print('dark fury ')
